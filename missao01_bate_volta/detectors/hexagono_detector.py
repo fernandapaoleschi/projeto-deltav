95012dcc212e949038859601a7e5933783ae8515
thiagoCaFeFi <141078848+thiagoCaFeFi@users.noreply.github.com>
2025-10-28 23:12:15 -0300

    Estrutura de Dados Atualização
    Detectores atualizados para implementarem informação no objeto da estrutura e o Centralization implementa mais informações no objeto também.

--- missao01_bate_volta/detectors/hexagono_detector.py
+++ missao01_bate_volta/detectors/hexagono_detector.py
@@ -1,14 +1,37 @@
 import cv2
 from detectors.base_detector import ShapeDetector
 from config import vision_config as settings
+from core.shape_data import ShapeData 
+from typing import Optional         
 
 class HexagonDetector(ShapeDetector):
     def __init__(self):
         super().__init__("hexagono")
 
-    def detect(self, contour):
-        #Detecta um hexágono verificando se o polígono aproximado tem 6 vértices
+    def detect(self, contour) -> Optional[ShapeData]: 
+        """Detecta um hexágono verificando se o polígono aproximado tem 6 vértices"""
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, settings.APPROX_POLY_FACTOR * peri, True)
         
-        return len(approx) == 6
+        if len(approx) == 6:
+            # SUCESSO! Coleta os dados básicos
+            area = cv2.contourArea(contour)
+            M = cv2.moments(contour)
+            
+            if M["m00"] == 0: 
+                return None # Evita divisão por zero
+            
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            bounding_box = cv2.boundingRect(contour)
+            
+            # Retorna o objeto ShapeData preenchido
+            return ShapeData(
+                name=self.name,
+                contour=contour,
+                center=center,
+                bounding_box=bounding_box,
+                area=area
+            )
+            
+        # Falha na detecção
+        return None
